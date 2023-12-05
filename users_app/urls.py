a1af9c1a494a8e44d2d95cd83de9442642a3feb3
mcstao <arislg99@gmail.com>
2023-12-06 02:38:54 +0600

    Added token endpoints

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from .views import RegisterUserView, LoginUserView, LogoutView, ConfirmEmailView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('confirm-email/<str:token>/', ConfirmEmailView.as_view(), name='confirm-email'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
 ]
